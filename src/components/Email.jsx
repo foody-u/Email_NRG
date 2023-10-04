807763d07152dc0deddf972778f4d0dd29065afa
warnsem <s3menchyk@yandex.ru>
2023-10-04 13:44:59 +0200

    pravki in footer

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -217,73 +217,151 @@ export const Email = ({ preview }) => (
                     <br />
 
                     <LineSeparator />
-                    <br />
+
                     <Section style={{
                     }}>
-                        <Row>
-                            <Column style={{
-                            }}>
-                                <Heading style={{
-                                    ...TEXT_STYLE.h2,
-                                    fontWeight: "Bold"
+                        <Column style={{
+                            height: "250px"
+                        }}>
+                            <Column>
+                                <div style={{
                                 }}>
-                                    Any questions?
-                                </Heading>
+                                    <Heading style={{
+                                        ...TEXT_STYLE.h3,
+                                        fontWeight: "Bold",
+                                    }}>
+                                        Any questions?
+                                    </Heading>
+                                </div>
 
-                                <Text style={{
-                                    ...TEXT_STYLE.textSm,
-                                    textAlign: "start",
+
+                                <div style={{
+                                    height: "80px"
                                 }}>
-                                    WhatsApp me at <Link
-                                        href='https://wa.me/447534580009'
-                                        style={{
-                                            fontWeight: "bold"
-                                        }}>07534580009</Link> or press
-                                    the button below
+                                    <Text style={{
+                                        ...TEXT_STYLE.textSm,
+                                        textAlign: "start",
+                                        width: "200px"
+                                    }}>
+                                        WhatsApp me at <Link
+                                            href='https://wa.me/447534580009'
+                                            style={{
+                                                fontWeight: "bold"
+                                            }}>07534580009</Link> or press
+                                        the button below
 
-                                </Text>
-                                <ButtonLink href='https://wa.me/447534580009'>Contact</ButtonLink>
-                                <ButtonLink
-                                    href="https://damirov.com#rec539368327"
-                                    marginLeft="25px">Reviews</ButtonLink>
+                                    </Text>
+                                </div>
+
+                                <div style={{
+                                    height: "20px"
+                                }}></div>
+                                <div style={{
+                                    height: "60px"
+                                }}>
+                                    <ButtonLink
+                                        padding="13px 20px"
+                                        background="#545454"
+                                        borderRadius="5px"
+                                        href='https://wa.me/447534580009'>Contact</ButtonLink>
+                                    <ButtonLink
+                                        padding="13px 20px"
+                                        background="#545454"
+                                        borderRadius="5px"
+                                        href="https://damirov.com#rec539368327"
+                                        marginLeft="25px">Reviews</ButtonLink>
+
+                                </div>
                             </Column>
 
-                        </Row>
-                        <Row style={{
-                            marginTop: "50px"
-                        }}>
+                        </Column>
 
-                            <Column>
+                        <Row>
+
+                            <Column style={{
+                                width: "110px"
+                            }}>
+                                <div style={{
+                                }}>
+                                    <Text style={{
+                                        ...TEXT_STYLE.h3,
+                                        fontWeight: "bold",
+                                        lineHeight: "35px",
+                                        margin: 0
+                                    }}>David Damirov</Text>
+                                </div>
+
+                                <div style={{
+                                    height: "80px"
+                                }}>
+                                    <Text style={{
+                                        ...TEXT_STYLE.textSm
+                                    }}>
+                                        Sport Nutritionist
+                                        <br />
+                                        Personal Trainer
+                                    </Text>
+                                </div>
+
+
+                                <Row>
+                                    <div style={{
+                                        height: "60px"
+                                    }}>
+
+                                        <Link
+
+                                            href="http://instagram.com/damirov">
+                                            <Img
+                                                style={{
+                                                    display: "inline"
+                                                }}
+                                                width={"30px"}
+                                                height={"30px"}
+                                                src='https://ik.imagekit.io/foodyuapp/NRG%20&%20Damirov/instagram.png?updatedAt=1696415422946'
+                                            />
+                                        </Link>
+
+                                        <Link
+                                            href="https://damirov.com">
+                                            <Img
+                                                style={{
+                                                    display: "inline",
+                                                    marginLeft: "20px"
+                                                }}
+                                                width={"30px"}
+                                                height={"30px"}
+                                                src='https://ik.imagekit.io/foodyuapp/NRG%20&%20Damirov/web%20icon.png?updatedAt=1696415433712'
+                                            />
+                                        </Link>
+                                    </div>
+                                </Row>
+
+
+                            </Column>
+
+                            <Column style={{
+                                width: "120px"
+                            }}>
                                 <Img
-                                    src='https://ik.imagekit.io/foodyuapp/NRG%20&%20Damirov/David%20Avatar.png?updatedAt=1696333812451'
-                                    width={"90px"}
-                                    height={"90px"}
+                                    src='https://ik.imagekit.io/foodyuapp/NRG%20&%20Damirov/David%20avatar.png?updatedAt=1696415451399'
+                                    width={"125px"}
+                                    height={"125px"}
                                     alt="Photo of David"
                                     style={{
-                                        marginRight: "-28px",
                                         marginTop: "-20px",
                                         borderRadius: "999px"
                                     }}
                                 />
+
                             </Column>
-                            <Column style={{
-                            }}>
-                                <Text style={{
-                                    ...TEXT_STYLE.h2
-                                }}>David Damirov</Text>
-                                <Text style={{
-                                    ...TEXT_STYLE.textSm
-                                }}>Sport Nutritionist</Text>
-                                <Text style={{
-                                    ...TEXT_STYLE.textSm
-                                }}>Personal trainer</Text>
-                            </Column>
+
+
+
+
                         </Row>
-                        <Row>
-                            <Link>
-                                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="55px" width="55px" xmlns="http://www.w3.org/2000/svg"><path d="M512 306.9c-113.5 0-205.1 91.6-205.1 205.1S398.5 717.1 512 717.1 717.1 625.5 717.1 512 625.5 306.9 512 306.9zm0 338.4c-73.4 0-133.3-59.9-133.3-133.3S438.6 378.7 512 378.7 645.3 438.6 645.3 512 585.4 645.3 512 645.3zm213.5-394.6c-26.5 0-47.9 21.4-47.9 47.9s21.4 47.9 47.9 47.9 47.9-21.3 47.9-47.9a47.84 47.84 0 0 0-47.9-47.9zM911.8 512c0-55.2.5-109.9-2.6-165-3.1-64-17.7-120.8-64.5-167.6-46.9-46.9-103.6-61.4-167.6-64.5-55.2-3.1-109.9-2.6-165-2.6-55.2 0-109.9-.5-165 2.6-64 3.1-120.8 17.7-167.6 64.5C132.6 226.3 118.1 283 115 347c-3.1 55.2-2.6 109.9-2.6 165s-.5 109.9 2.6 165c3.1 64 17.7 120.8 64.5 167.6 46.9 46.9 103.6 61.4 167.6 64.5 55.2 3.1 109.9 2.6 165 2.6 55.2 0 109.9.5 165-2.6 64-3.1 120.8-17.7 167.6-64.5 46.9-46.9 61.4-103.6 64.5-167.6 3.2-55.1 2.6-109.8 2.6-165zm-88 235.8c-7.3 18.2-16.1 31.8-30.2 45.8-14.1 14.1-27.6 22.9-45.8 30.2C695.2 844.7 570.3 840 512 840c-58.3 0-183.3 4.7-235.9-16.1-18.2-7.3-31.8-16.1-45.8-30.2-14.1-14.1-22.9-27.6-30.2-45.8C179.3 695.2 184 570.3 184 512c0-58.3-4.7-183.3 16.1-235.9 7.3-18.2 16.1-31.8 30.2-45.8s27.6-22.9 45.8-30.2C328.7 179.3 453.7 184 512 184s183.3-4.7 235.9 16.1c18.2 7.3 31.8 16.1 45.8 30.2 14.1 14.1 22.9 27.6 30.2 45.8C844.7 328.7 840 453.7 840 512c0 58.3 4.7 183.2-16.2 235.8z"></path></svg>
-                            </Link>
-                        </Row>
+
+
                     </Section>
                 </Section>
 
@@ -391,21 +469,25 @@ function PriceButton({
                 padding: 'none',
             }}>
                 £{price}
-                {oldPrice && <>
-                    <Text style={{
-                        ...TEXT_STYLE.textSm,
-                        padding: 'none',
-                        lineHeight: 0,
-                    }}>
-                        <strike>£{oldPrice}</strike>
-                        {" "}· you save £{oldPrice - price}
-                    </Text>
-                </>}
+
+                <div style={{
+                    height: "50px"
+                }}>
+                    {oldPrice && <>
+                        <Text style={{
+                            ...TEXT_STYLE.textSm,
+                            padding: 'none',
+                            lineHeight: 0,
+                        }}>
+                            <strike>£{oldPrice}</strike>
+                            {" "}· you save £{oldPrice - price}
+                        </Text>
+                    </>}
+                </div>
             </Text>
 
             <CustomLink
                 href={href}
-                marginTop={!oldPrice ? "55px" : "40px"}
                 small
                 {...styles}
             >{text}</CustomLink>
